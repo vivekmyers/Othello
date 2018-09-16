23eee4fc3cafef6651319c70313f76007d95eaa0
Vivek Myers <vivek.myers@gmail.com>
2018-09-16 04:20:20 -0400

    More Training

--- visual.py
+++ visual.py
@@ -3,10 +3,15 @@ import algorithm
 import tqdm
 import neural
 import tqdm
+import tkinter
 
-game.play_gui(
-    algorithm.stochastic_minimax(neural.heuristic, 5),
-    algorithm.stochastic_minimax(algorithm.basic_heuristic, 5),
-    delay=1,
-    heuristic=neural.heuristic,
-)
+try:
+    game.play_gui(
+        algorithm.stochastic_minimax(neural.heuristic, 5),
+        algorithm.stochastic_minimax(algorithm.basic_heuristic, 5),
+        delay=1,
+        heuristic=neural.heuristic,
+    )
+except (tkinter.TclError, KeyboardInterrupt, EOFError):
+    print('\nQuit')
+    exit(1)
